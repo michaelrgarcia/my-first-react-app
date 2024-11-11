e65d578e02deacb15b56bdc54ef419ab176cc38d
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-11-10 22:34:13 -0800

    Link Greeting + GreetingInfo components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Greeting, GreetingInfo } from "./Greeting.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Greeting />
+    <GreetingInfo />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
